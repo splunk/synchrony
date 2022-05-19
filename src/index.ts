74ad6d269f31ef6aef3f0deebdd2647983bac757
Greg Wittel <greg@twinwave.io>
2022-05-19 16:49:06 -0700

    Expose alternate interface so obfuscations are returned
    Existing API behavior preserved, main new API is
    
    * deobfuscator.deobfuscateSourceWithDetails

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
-import { Deobfuscator, DeobfuscateOptions } from './deobfuscator'
+import {
+  Deobfuscator,
+  DeobfuscateOptions,
+  DeobfuscationResult,
+  DeobfuscateNodeResult,
+} from './deobfuscator'
 import { Transformer, TransformerOptions } from './transformers/transformer'
+import Context from './context'
 
-export { Deobfuscator, DeobfuscateOptions, Transformer, TransformerOptions }
+export {
+  Deobfuscator,
+  DeobfuscateOptions,
+  DeobfuscationResult,
+  DeobfuscateNodeResult,
+  Transformer,
+  TransformerOptions,
+  Context,
+}
